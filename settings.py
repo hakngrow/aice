d2eaa4cd2c6c5d3d21df5f2d5201eb1e030ceca7
hakngrow <56512250+hakngrow@users.noreply.github.com>
2020-04-22 22:01:49 +0800

    Created settings.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+"""
+Default script settings
+"""
+
+DB_HOST = 'aice.database.windows.net'
+DB_NAME = 'aice'
+DB_PORT = '1433'
+DB_USERNAME = 'aice_candidate'
+DB_PASSWORD = '@ic3_a3s0c1at3'
+
+TBL_RENTAL = 'rental_data'
